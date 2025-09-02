1096e2c5f173dded8cb785fba424906a3e7d3a0a
adi-adisucipto <suciptoadi550@gmail.com>
2025-09-02 12:08:04 +0700

    Menambahkan file dataType.js

---
+++ module-01/day-01/dataType.js
@@ -0,0 +1,10 @@
+// TIPE DATA STRING (TEXT)
+var text = "Hello";
+console.log(text);
+
+// TIPE DATA NUMBER (ANGKA)
+var number = 10;
+console.log(number + 5);
+
+// TIPE DATA BOOLEAN (TRUE / FALSE)
+var boolen = false;
