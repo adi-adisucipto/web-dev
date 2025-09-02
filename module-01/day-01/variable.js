b47025a66453dbe26fb86a18141898d53ff72749
adi-adisucipto <suciptoadi550@gmail.com>
2025-09-02 12:12:51 +0700

    Menambahkan file variable.js

---
+++ module-01/day-01/variable.js
@@ -0,0 +1,31 @@
+// const readline = require("readline");
+
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+// });
+
+// rl.question("Masukkan Panjang:  ", (answer) => {
+    
+//     rl.close();
+// });
+
+let panjang;
+let lebar;
+
+panjang = 5;
+lebar = 7
+
+const luasPersegiPanjang = panjang * lebar;
+
+// console.log(luasPersegiPanjang);
+
+let now = new Date();
+// console.log(now);
+let date = new Date("2022-01-01");
+// console.log(date);
+
+let text = "5";
+console.log(Number(text) + 5);
+
+// concat itu menggabungkan string dengan string : "5" + "5" = "55"
