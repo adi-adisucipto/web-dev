5f389c29fe35772eb43fe0418f7838c7ebd251dc
adi-adisucipto <suciptoadi550@gmail.com>
2025-09-02 12:10:13 +0700

    Menambahkan file buildInMethod.js

---
+++ module-01/day-01/buildInMethod.js
@@ -0,0 +1,47 @@
+// String
+
+// slice
+// const text = "Adi Sucipto";
+// console.log(text.slice(1, 3));
+
+// substring
+// const text = "Adi Sucipto";
+// console.log(text.substring(1, 3));
+
+// Replace
+// const text = "Adi Sucipto";
+// console.log(text.replace("Sucipto", "Barkri"));
+
+// replaceAll
+// let kalimat = "Saya suka Apel. Apel itu enak";
+// let hasil = kalimat.replaceAll("Apel", "Jeruk");
+// console.log(hasil);
+
+// split
+let kalimat = "Adi Sucipto";
+// console.log(kalimat.split(" "));
+
+// trim
+// console.log(kalimat.trim());
+
+// toUpperCase
+// console.log(kalimat.toUpperCase());
+
+// toLowerCase
+// console.log(kalimat.toLowerCase());
+
+// charAt
+// console.log(kalimat.charAt(4));
+
+// length
+// console.log(kalimat.length);
+
+// indexOf
+// console.log(kalimat.indexOf("Sucipto"));
+
+// lastIndexOf
+let kalimat2 = "Nama kamu Adi. Hai, Adi!";
+// console.log(kalimat2.lastIndexOf("Adi"));
+
+// includes
+console.log(kalimat2.includes("di"));
